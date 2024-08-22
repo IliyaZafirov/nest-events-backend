b4040c654bfe4370f75745ca037e3c6419663407
Shaker <133087050+IliyaZafirov@users.noreply.github.com>
2024-08-22 19:51:50 +0300

    /practice endpoint explained as comment

--- src/events.controller.ts
+++ src/events.controller.ts
@@ -19,7 +19,7 @@ export class EventsController {
     async findAll() {
         return await this.repository.find()
     }
-
+    // SELECT * FROM event WHERE (event.id > 3 AND event.when > '2021-02-12T13:00:00') OR event.description LIKE '%meet%' LIMIT 2
     @Get('/practice')
     async practice() {
         return await this.repository.find({
