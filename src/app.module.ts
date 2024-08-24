e3f694d091d3f0eeac83005e212a6fb4ebaa1698
Shaker <133087050+IliyaZafirov@users.noreply.github.com>
2024-08-24 10:17:45 +0300

    dynamic custom config for prod/dev added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,21 +3,22 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EventsModule } from './events/events.module';
 import { ConfigModule } from '@nestjs/config';
+import ormConfig from './config/orm.config';
+import ormConfigProd from './config/orm.config.prod';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: process.env.DB_HOST,
-    port: +process.env.DB_PORT,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    entities: [Event],
-    synchronize: true
-  }),
-  EventsModule
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [ormConfig],
+      expandVariables: true
+      // envFilePath: '.env'
+      // ignoreEnvFile: true
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: process.env.NODE_ENV !== 'production' ? ormConfig : ormConfigProd
+    }),
+    EventsModule
   ],
   providers: [AppService]
 })
